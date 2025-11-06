ab4b839157de373f31a37b4c541f68939c72e557
Harshit <moksh9355@gmail.com>
2025-11-06 20:47:14 +0530

    Fixed MIME issue and base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '', // ✅ leave empty for Vercel or GitHub Pages auto-path
+  build: {
+    outDir: 'dist', // ensures build goes to dist
+  },
+  server: {
+    open: true,
+  },
 })
